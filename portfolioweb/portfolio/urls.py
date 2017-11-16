310e962e861aee64a13d49bb16aab208bdfd7651
Granton Mchana <gmchana254@gmail.com>
2017-11-16 16:01:07 +0300

    fixing bugs

--- portfolioweb/portfolio/urls.py
+++ portfolioweb/portfolio/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
-    url('^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^project/(\d+)'), views.project, name = 'project'),
 ]
--- portfolioweb/portfolio/views.py
+++ portfolioweb/portfolio/views.py
@@ -3,9 +3,20 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Welcome to Portfolio Website')
+    projects = Project.get_projects()
+    return render(request, 'all-projects/index.html', {"projects": projects})
+
+
+def project(request, project - id):
+
+    try:
+        project = Project.objects.get(id=project_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request, 'all-projects/project.html', {"project": project})
